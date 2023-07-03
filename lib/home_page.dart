5c4d5cd5d9feeb34ae1463aeed1a104a70587415
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-03 23:26:10 +0530

    new pages

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,22 +7,50 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: const [
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: Center(
+            child: Text(
+              'Medlogs',
+              style: TextStyle(
+                  fontFamily: 'Titan_One',
+                  color: Color(0xffEE4B2B),
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
         Padding(
-            padding: EdgeInsets.symmetric(verical: 5),
-            child: Centre(
-                child: Text('Medlogs',
-                    style: TextStyle(
-                      fontFamily: 'Titan_One',
-                      color: Color(0xffEE4B2B),
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    ),
-                    ),
-                    ),
-                    )
-                    Padding(
-                      padding:const EdgeInsets.symmetric(horizontal:10,vertical:20),)
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                width: 4.0,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.asset('images/Logo.jpg')),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TypeCard(
+              typename: 'Medicine',
+              page: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MedicinePage()));
+              },
+            ),
+            TypeCard(
+              typeName: 'Medical Reports',
+            )
+          ],
+        ),
       ],
     );
   }
