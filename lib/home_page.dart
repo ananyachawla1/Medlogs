ca1aca5f71ec4de51c9f0a88ff5204116684f957
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-04 00:33:40 +0530

    updated medicine card

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -43,7 +43,7 @@ class HomePage extends StatelessWidget {
               typename: 'Medicine',
               page: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MedicinePage()));
+                    MaterialPageRoute(builder: (context) => MedicineCard()));
               },
             ),
             TypeCard(
--- lib/widget/medicine_card.dart
+++ lib/widget/medicine_card.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
 
+class MedicineCard extends StatelessWidget {
+  const MedicineCard(
+      {required this.medicine, required this.time, required this.quantity});
+  final int time;
+  final String medicine;
+  final int quantity;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15.0),
+            border: Border.all(color: Colors.grey.withOpacity(.01)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                offset: const Offset(0, 0.5),
+                spreadRadius: .1,
+                blurRadius: 3,
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.medication_rounded),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Allegra-M',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
+                  Text('Before Sleeping')
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
